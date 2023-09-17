68b559fd66e38fc8e95c77fa6344905f0d6ba402
lecodeduchat <lecodeduchat@gmail.com>
2023-09-17 20:28:54 +0200

    Ajout des pages projets dans le dossier react-vitrine

--- react-vitrine/src/pages/Contact.js
+++ react-vitrine/src/pages/Contact.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
-    return (
-        <h1>Contact</h1>
-    );
+  return (
+    <main>
+      <Mouse />
+      <div className="contact">
+        <Navigation />
+        <Logo />
+        <ContactForm />
+      </div>
+    </main>
+  );
 };
 
 export default Contact;
--- react-vitrine/src/pages/Project4.js
+++ react-vitrine/src/pages/Project4.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+import Buttons from "../components/Buttons";
+import Project from "../components/Project";
 
 const Project4 = () => {
-    return (
-        <h1>Projet 4</h1>
-    );
+  return (
+    <main>
+      <Mouse />
+      <div className="project">
+        <Navigation />
+        <Logo />
+        <Project projectNumber={3} />
+        <Buttons left={"/projet-3"} />
+      </div>
+    </main>
+  );
 };
 
 export default Project4;
